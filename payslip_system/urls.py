86d7926deef6317de75d7bc57d0fd60d9edb6336
adithyaaneesh <adithyaaneesh29@gmail.com>
2026-01-08 15:55:28 +0530

    final commit

--- payslip_system/urls.py
+++ payslip_system/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path("",views.employee_list, name='employee_list'),
     path('add/', views.add_employee, name='add_employee'),
     path('edit/<int:emp_id>/', views.update_employee, name='edit_employee'),
     path('delete/<int:emp_id>/', views.delete_employee, name='delete_employee'),
-    path("",views.employee_list, name='employee_list'),
-    path('salary/<int:emp_id>/', views.add_or_edit_salary, name='add_salary'),
     path("payslip_view/<int:emp_id>/",views.payslip_view, name='payslip_view'),
     
 ]
--- payslip_system/views.py
+++ payslip_system/views.py
@@ -4,25 +4,46 @@ from .models import Employee, Salary
 # Create your views here.
 
 def add_employee(request):
-    if request.method == "POST":
-        forms = EmployeeForm(request.POST)
-        if forms.is_valid():
-            forms.save()
-            return redirect('employee_list')
-    else:
-        forms = EmployeeForm()
-    return render(request, 'add_employee.html', {'form': forms})
-
-def update_employee(request,emp_id):
-    employee = get_object_or_404(Employee,id=emp_id)
-    if request.method == "POST":
-        form = EmployeeForm(request.POST, instance=employee)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
         if form.is_valid():
-            form.save()
+            employee = form.save()
+        return redirect('edit_employee', emp_id=employee.id)
+    else:
+        form = EmployeeForm()
+    return render(request, 'add_employee.html', {'form': form})
+
+
+
+def update_employee(request, emp_id):
+    employee = get_object_or_404(Employee, id=emp_id)
+    salary, created = Salary.objects.get_or_create(
+        employee=employee,
+        defaults={
+            'hra': 0,
+            'da': 0,
+            'other_allowance': 0,
+            'deduction': 0,
+            'leave_days': 0
+        }
+    )
+    if request.method == "POST":
+        emp_form = EmployeeForm(request.POST, instance=employee)
+        sal_form = SalaryForm(request.POST, instance=salary)
+        if emp_form.is_valid() and sal_form.is_valid():
+            emp_form.save()
+            sal_form.save()
             return redirect('employee_list')
     else:
-        form = EmployeeForm(instance=employee)
-    return render(request, 'edit_employee.html', {'form':form})
+        emp_form = EmployeeForm(instance=employee)
+        sal_form = SalaryForm(instance=salary)
+
+    return render(request, 'edit_employee.html', {
+        'emp_form': emp_form,
+        'sal_form': sal_form,
+        'employee': employee
+    })
+
 
 def delete_employee(request, emp_id):
     employee = get_object_or_404(Employee, id=emp_id)
@@ -34,37 +55,9 @@ def employee_list(request):
     employees = Employee.objects.all()
     return render(request,'employee_list.html', {'employees': employees})
 
-def add_or_edit_salary(request, emp_id):
-    employee = get_object_or_404(Employee, id=emp_id)
-
-    salary, created = Salary.objects.get_or_create(
-        employee=employee,
-        defaults={
-            'hra': 0,
-            'da': 0,
-            'other_allowance': 0,
-            'deduction': 0,
-            'leave_days': 0
-        }
-    )
-
-    if request.method == "POST":
-        form = SalaryForm(request.POST, instance=salary)
-        if form.is_valid():
-            form.save()
-            return redirect('payslip_view', emp_id=emp_id)
-    else:
-        form = SalaryForm(instance=salary)
-
-    return render(request, 'add_salary.html', {
-        'form': form,
-        'employee': employee
-    })
-
 def payslip_view(request, emp_id):
     employee = get_object_or_404(Employee, id=emp_id)
     salary = get_object_or_404(Salary, employee=employee)
-
     return render(request, 'payslip.html', {
         'employee': employee,
         'salary': salary
